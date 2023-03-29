74e5f815571b0cc2d6a399a0dbee0d17b75ffbb5
Luis Pamplona <luispamplona2702@gmail.com>
2023-03-28 23:10:19 -0300

    Criado site que mostra o pokemon da pokebola

---
+++ script/script.js
@@ -0,0 +1,56 @@
+function charmander(){
+
+    
+
+    var div = document.getElementById("pokemon")
+
+    var imagem = document.createElement('img')
+
+    imagem.src = "./image/char.png"
+
+    div.appendChild(imagem)
+
+    
+}
+function bulba(){
+
+    
+
+    var div = document.getElementById("pokemon")
+
+    var imagem = document.createElement('img')
+
+    imagem.src = "./image/bulba.png"
+
+    div.appendChild(imagem)
+
+    
+}
+function squirtle(){
+
+    
+
+    var div = document.getElementById("pokemon")
+
+    var imagem = document.createElement('img')
+
+    imagem.src = "./image/squi.png"
+
+    div.appendChild(imagem)
+
+    
+}
+function pikachu(){
+
+    
+
+    var div = document.getElementById("pokemon")
+
+    var imagem = document.createElement('img')
+
+    imagem.src = "./image/pika.png"
+
+    div.appendChild(imagem)
+
+    
+}
